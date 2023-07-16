0ed47cf8aed3c264dcb5d1c3afa593ca1c3a3908
Rupok Koiry <koiry.rupok@gmail.com>
2023-07-16 17:58:09 +0600

    UI Updated

--- src/components/codeEditor/CodeEditor.jsx
+++ src/components/codeEditor/CodeEditor.jsx
@@ -1,66 +1,20 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useCallback, useContext, useEffect, useState } from "react";
 import Editor from "@monaco-editor/react";
 import GlobalContext from "../../contexts/Global-Context";
 import "./code-editor.scss";
-import { Link, NavLink, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import axios from "axios";
+import { htmlSuggestions, cssSuggestions } from "../../utils/suggestions";
+import PropTypes from "prop-types";
 
-const htmlSuggestions = [
-  { label: "div", snippet: "<div>$1</div>" },
-  { label: "span", snippet: "<span>$1</span>" },
-  { label: "p", snippet: "<p>$1</p>" },
-  { label: "h1", snippet: "<h1>$1</h1>" },
-  { label: "h2", snippet: "<h2>$1</h2>" },
-  { label: "h3", snippet: "<h3>$1</h3>" },
-  { label: "h4", snippet: "<h4>$1</h4>" },
-  { label: "h5", snippet: "<h5>$1</h5>" },
-  { label: "h6", snippet: "<h6>$1</h6>" },
-  { label: "a", snippet: '<a href="$1">$2</a>' },
-  { label: "img", snippet: '<img src="$1" alt="$2">' },
-  { label: "ul", snippet: "<ul>\n\t<li>$1</li>\n</ul>" },
-  { label: "ol", snippet: "<ol>\n\t<li>$1</li>\n</ol>" },
-  { label: "li", snippet: "<li>$1</li>" },
-  {
-    label: "table",
-    snippet: "<table>\n\t<tr>\n\t\t<td>$1</td>\n\t</tr>\n</table>",
-  },
-  { label: "tr", snippet: "<tr>\n\t<td>$1</td>\n</tr>" },
-  { label: "td", snippet: "<td>$1</td>" },
-];
-
-const cssSuggestions = [
-  { label: "width", snippet: "width: $1;" },
-  { label: "height", snippet: "height: $1;" },
-  { label: "background-color", snippet: "background-color: $1;" },
-  { label: "color", snippet: "color: $1;" },
-  { label: "font-size", snippet: "font-size: $1;" },
-  { label: "font-weight", snippet: "font-weight: $1;" },
-  { label: "text-align", snippet: "text-align: $1;" },
-  { label: "border", snippet: "border: $1;" },
-  { label: "padding", snippet: "padding: $1;" },
-  { label: "margin", snippet: "margin: $1;" },
-  { label: "display", snippet: "display: $1;" },
-  { label: "flex-direction", snippet: "flex-direction: $1;" },
-  { label: "justify-content", snippet: "justify-content: $1;" },
-  { label: "align-items", snippet: "align-items: $1;" },
-  { label: "position", snippet: "position: $1;" },
-  { label: "top", snippet: "top: $1;" },
-  { label: "right", snippet: "right: $1;" },
-  { label: "bottom", snippet: "bottom: $1;" },
-  { label: "left", snippet: "left: $1;" },
-  { label: "z-index", snippet: "z-index: $1;" },
-  { label: "opacity", snippet: "opacity: $1;" },
-  { label: "border-radius", snippet: "border-radius: $1;" },
-  { label: "transition", snippet: "transition: $1;" },
-];
 const CodeEditor = ({ selectedLanguage, code, answers, onChange }) => {
-  const { language, topic, exerciseNum } = useParams();
+  const { topic, exerciseNum } = useParams();
   const { isDarkMode } = useContext(GlobalContext);
   const [value, setValue] = useState(code || "");
-  const [submittedValue, setSubmittedValue] = useState("");
-  const [submittedCode, setSubmittedCode] = useState("");
-  const [submittedAnswer, setSubmittedAnswer] = useState("");
+  const [submittedAnswer, setSubmittedAnswer] = useState({});
   const [editorInstance, setEditorInstance] = useState(null);
+  const ctrlPress = useKeyPress("Control");
+
   const handleEditorChange = (value) => {
     setValue(value);
     onChange(value);
@@ -69,26 +23,27 @@ const CodeEditor = ({ selectedLanguage, code, answers, onChange }) => {
   function parseInput(input) {
     const lines = input.split("\n");
 
-    let questionLines = [];
-    let codeLines = [];
+    const questionLines = [];
+    const codeLines = [];
 
-    for (let line of lines) {
+    for (const line of lines) {
       if (line.trim().startsWith("//")) {
-        // This is a line of the question
-        questionLines.push(line.replace("//", "").trim());
+        const questionLine = line.replace("//", "").trim();
+        questionLines.push(questionLine);
       } else {
-        // This is a line of the code
         codeLines.push(line);
       }
     }
 
-    // Join the lines without adding a newline at the end
-    let question = questionLines.join("\n");
-    let code = codeLines.join("\n");
+    const question = questionLines.join("\n");
+    const code = codeLines.join("\n");
 
     return { question, code };
   }
   const handleSubmitValue = async () => {
+    if (!parseInput(value).code.trim())
+      return setSubmittedAnswer({ isCorrect: false, score: 0 });
+
     try {
       const response = await axios.post(
         "https://api.openai.com/v1/chat/completions",
@@ -107,33 +62,16 @@ const CodeEditor = ({ selectedLanguage, code, answers, onChange }) => {
         {
           headers: {
             "Content-Type": "application/json",
-            Authorization:
-              "Bearer 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",
+            Authorization: `Bearer ${import.meta.env.VITE_APP_GPT_KEY}`,
           },
         }
       );
 
-      const ans = Function(
+      const result = Function(
         `"use strict"; return (${response.data.choices[0].message.content});`
       )();
-      alert(
-        `Your answer is ${
-          ans.isCorrect ? "correct" : "incorrect"
-        } with a score of ${ans.score}`
-      );
-      // setSubmittedAnswer(ans);
-      /* answers.map(
-        (answer) =>
-          value
-            .toString()
-            .replace(/\n/g, "")
-            .replace(/ /g, "")
-            .indexOf(
-              answer.code.toString().replace(/\n/g, "").replace(/ /g, "")
-            ) > 0 && setSubmittedAnswer(answer)
-      ); */
+      setSubmittedAnswer(result);
     } catch (error) {
-      console.log(error);
       alert("Please try after 30 seconds");
     }
   };
@@ -161,37 +99,23 @@ const CodeEditor = ({ selectedLanguage, code, answers, onChange }) => {
       },
     });
   };
-  function handleMouseWheel(event, monaco) {
-    if (editorInstance && event.ctrlKey) {
-      event.preventDefault();
-      const zoomIn = event.deltaY < 0;
-      const increment = zoomIn ? 1 : -1;
-      const currentFontSize = editorInstance.getOption(
-        monaco.editor.EditorOption.fontSize
-      );
-      const newFontSize = currentFontSize + increment;
-      editorInstance.updateOptions({ fontSize: newFontSize });
-    }
-  }
-
-  const ctrlPress = useKeyPress("Control");
 
   function useKeyPress(targetKey) {
     const [keyPressed, setKeyPressed] = useState(false);
 
-    function downHandler({ key }) {
-      if (key === targetKey) {
-        setKeyPressed(true);
-      }
-    }
-
-    const upHandler = ({ key }) => {
-      if (key === targetKey) {
-        setKeyPressed(false);
-      }
-    };
-
     useEffect(() => {
+      function downHandler({ key }) {
+        if (key === targetKey) {
+          setKeyPressed(true);
+        }
+      }
+
+      const upHandler = ({ key }) => {
+        if (key === targetKey) {
+          setKeyPressed(false);
+        }
+      };
+
       window.addEventListener("keydown", downHandler);
       window.addEventListener("keyup", upHandler);
 
@@ -199,11 +123,26 @@ const CodeEditor = ({ selectedLanguage, code, answers, onChange }) => {
         window.removeEventListener("keydown", downHandler);
         window.removeEventListener("keyup", upHandler);
       };
-    }, []); // Empty array ensures that effect is only run on mount and unmount
+    }, [targetKey]);
 
     return keyPressed;
   }
 
+  const handleMouseWheel = useCallback(
+    (event, monaco) => {
+      if (editorInstance && event.ctrlKey) {
+        event.preventDefault();
+        const zoomIn = event.deltaY < 0;
+        const increment = zoomIn ? 1 : -1;
+        const currentFontSize = editorInstance.getOption(
+          monaco.editor.EditorOption.fontSize
+        );
+        const newFontSize = currentFontSize + increment;
+        editorInstance.updateOptions({ fontSize: newFontSize });
+      }
+    },
+    [editorInstance]
+  );
   useEffect(() => {
     if (editorInstance) {
       editorInstance
@@ -217,7 +156,7 @@ const CodeEditor = ({ selectedLanguage, code, answers, onChange }) => {
           .removeEventListener("wheel", handleMouseWheel);
       }
     };
-  }, [editorInstance, ctrlPress]);
+  }, [editorInstance, ctrlPress, handleMouseWheel]);
 
   const options = {
     selectOnLineNumbers: true,
@@ -234,7 +173,7 @@ const CodeEditor = ({ selectedLanguage, code, answers, onChange }) => {
     formatOnPaste: true,
   };
   return (
-    <>
+    <React.Fragment>
       <Editor
         height={`${selectedLanguage === "javascript" ? "50vh" : "100%"}`}
         width={`${selectedLanguage === "javascript" ? "70%" : "100%"}`}
@@ -248,8 +187,8 @@ const CodeEditor = ({ selectedLanguage, code, answers, onChange }) => {
       {selectedLanguage === "javascript" && (
         <div
           className={`output-window ${
-            submittedAnswer.length !== 0
-              ? submittedAnswer
+            submittedAnswer.isCorrect !== undefined
+              ? submittedAnswer.isCorrect
                 ? "success"
                 : "error"
               : ""
@@ -262,21 +201,22 @@ const CodeEditor = ({ selectedLanguage, code, answers, onChange }) => {
             >
               Run
             </span>
-            <span className="output-message">
-              {submittedAnswer.length !== 0
-                ? submittedAnswer
+            {submittedAnswer.isCorrect !== undefined && (
+              <span className="output-message">
+                {submittedAnswer.isCorrect
                   ? "Amazing, great job"
-                  : "Wrong answer, but you got this"
-                : ""}
-            </span>
+                  : "Wrong answer, but you got this"}
+              </span>
+            )}
           </div>
           <h3>Output / Console</h3>
           <hr className="horizontal-line" />
           <div className="output-window__content">
-            {submittedAnswer && (
+            {submittedAnswer && submittedAnswer.isCorrect !== undefined && (
               <p>
-                Your answer was {submittedAnswer.code}, it's score is:{" "}
-                {submittedAnswer.score}
+                Your answer was{" "}
+                <code>{submittedAnswer.isCorrect ? "Correct" : "Wrong"}</code>.
+                Its score is: {submittedAnswer.score}
               </p>
             )}
           </div>
@@ -285,8 +225,14 @@ const CodeEditor = ({ selectedLanguage, code, answers, onChange }) => {
           </a>
         </div>
       )}
-    </>
+    </React.Fragment>
   );
 };
 
+/* CodeEditor.propTypes = {
+  selectedLanguage: PropTypes.string,
+  code: PropTypes.string,
+  answers: PropTypes.arrayOf(PropTypes.string),
+  onChange: PropTypes.func,
+}; */
 export default CodeEditor;
