97930c6ae81884371cd9fb91dd65fd26767669dd
Rupok Koiry <koiry.rupok@gmail.com>
2023-09-08 20:18:05 +0600

    Quiz Added

--- src/components/quiz/Quiz.jsx
+++ src/components/quiz/Quiz.jsx
@@ -15,72 +15,72 @@ const questions = [
     ],
     correctAnswer: "console.log('Hello World')",
   },
-  // {
-  //   question: "Which symbol is used for comments in JavaScript?",
-  //   options: ["//", "/* */", "##", "--"],
-  //   correctAnswer: "//",
-  // },
-  // {
-  //   question: "How do you declare a variable in JavaScript?",
-  //   options: ["var x", "int x", "x", "declare x"],
-  //   correctAnswer: "var x",
-  // },
-  // {
-  //   question: "How do you create a function in JavaScript?",
-  //   options: [
-  //     "function myFunction()",
-  //     "def myFunction()",
-  //     "create myFunction()",
-  //     "function = myFunction()",
-  //   ],
-  //   correctAnswer: "function myFunction()",
-  // },
-  // {
-  //   question: "What is the correct way to write an array in JavaScript?",
-  //   options: [
-  //     "var colors = (1:'red', 2:'green', 3:'blue')",
-  //     "var colors = 'red', 'green', 'blue'",
-  //     "var colors = ['red', 'green', 'blue']",
-  //     "var colors = 1 = ('red'), 2 = ('green'), 3 = ('blue')",
-  //   ],
-  //   correctAnswer: "var colors = ['red', 'green', 'blue']",
-  // },
-  // {
-  //   question:
-  //     "How do you write a conditional statement for executing some code if 'i' is NOT equal to 5?",
-  //   options: ["if i =! 5 then", "if i <> 5", "if (i != 5)", "if (i not 5)"],
-  //   correctAnswer: "if (i != 5)",
-  // },
-  // {
-  //   question: "How does a FOR loop start?",
-  //   options: [
-  //     "for i = 1 to 5",
-  //     "for (i <= 5; i++)",
-  //     "for (i = 0; i <= 5)",
-  //     "for (i = 0; i <= 5; i++)",
-  //   ],
-  //   correctAnswer: "for (i = 0; i <= 5; i++)",
-  // },
-  // {
-  //   question: "What is the correct way to write a JavaScript array?",
-  //   options: [
-  //     "var txt = new Array(1:'tim',2:'kim',3:'jim')",
-  //     "var txt = new Array='tim','kim','jim'",
-  //     "var txt = new Array('tim','kim','jim')",
-  //     "var txt = new Array:1=('tim'),2=('kim'),3=('jim')",
-  //   ],
-  //   correctAnswer: "var txt = new Array('tim','kim','jim')",
-  // },
-  // {
-  //   question: "Which event occurs when the user clicks on an HTML element?",
-  //   options: ["onchange", "onmouseclick", "onmouseover", "onclick"],
-  //   correctAnswer: "onclick",
-  // },
-  // {
-  //   question: "What will the following code return: Boolean(10 > 9)?",
-  //   options: ["NaN", "false", "true", "undefined"],
-  //   correctAnswer: "true",
-  // },
+  {
+    question: "Which symbol is used for comments in JavaScript?",
+    options: ["//", "/* */", "##", "--"],
+    correctAnswer: "//",
+  },
+  {
+    question: "How do you declare a variable in JavaScript?",
+    options: ["var x", "int x", "x", "declare x"],
+    correctAnswer: "var x",
+  },
+  {
+    question: "How do you create a function in JavaScript?",
+    options: [
+      "function myFunction()",
+      "def myFunction()",
+      "create myFunction()",
+      "function = myFunction()",
+    ],
+    correctAnswer: "function myFunction()",
+  },
+  {
+    question: "What is the correct way to write an array in JavaScript?",
+    options: [
+      "var colors = (1:'red', 2:'green', 3:'blue')",
+      "var colors = 'red', 'green', 'blue'",
+      "var colors = ['red', 'green', 'blue']",
+      "var colors = 1 = ('red'), 2 = ('green'), 3 = ('blue')",
+    ],
+    correctAnswer: "var colors = ['red', 'green', 'blue']",
+  },
+  {
+    question:
+      "How do you write a conditional statement for executing some code if 'i' is NOT equal to 5?",
+    options: ["if i =! 5 then", "if i <> 5", "if (i != 5)", "if (i not 5)"],
+    correctAnswer: "if (i != 5)",
+  },
+  {
+    question: "How does a FOR loop start?",
+    options: [
+      "for i = 1 to 5",
+      "for (i <= 5; i++)",
+      "for (i = 0; i <= 5)",
+      "for (i = 0; i <= 5; i++)",
+    ],
+    correctAnswer: "for (i = 0; i <= 5; i++)",
+  },
+  {
+    question: "What is the correct way to write a JavaScript array?",
+    options: [
+      "var txt = new Array(1:'tim',2:'kim',3:'jim')",
+      "var txt = new Array='tim','kim','jim'",
+      "var txt = new Array('tim','kim','jim')",
+      "var txt = new Array:1=('tim'),2=('kim'),3=('jim')",
+    ],
+    correctAnswer: "var txt = new Array('tim','kim','jim')",
+  },
+  {
+    question: "Which event occurs when the user clicks on an HTML element?",
+    options: ["onchange", "onmouseclick", "onmouseover", "onclick"],
+    correctAnswer: "onclick",
+  },
+  {
+    question: "What will the following code return: Boolean(10 > 9)?",
+    options: ["NaN", "false", "true", "undefined"],
+    correctAnswer: "true",
+  },
 ];
 
 function Quiz() {
