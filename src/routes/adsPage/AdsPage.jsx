f0028bda7b2858a8be561957a27399eaef8faba2
Rupok Koiry <koiry.rupok@gmail.com>
2023-10-26 18:36:44 +0600

    Testing Adds

--- src/routes/adsPage/AdsPage.jsx
+++ src/routes/adsPage/AdsPage.jsx
@@ -1,27 +1,34 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
+export default function Banner() {
+  const banner = useRef();
 
-const AdsPage = () => {
-  const banner = useRef(HTMLDivElement);
-
+  const atOptions = {
+    key: "c721be1582bda356d78a40848af09f44",
+    format: "iframe",
+    height: 250,
+    width: 300,
+    params: {},
+  };
   useEffect(() => {
-    if (!banner.current.firstChild) {
+    if (banner.current && !banner.current.firstChild) {
+      const conf = document.createElement("script");
       const script = document.createElement("script");
       script.type = "text/javascript";
-      script.src = `//www.highcpmcreativeformat.com/c721be1582bda356d78a40848af09f44/invoke.js`;
+      script.src = `//www.highperformancedformats.com/c721be1582bda356d78a40848af09f44/invoke.js`;
+      conf.innerHTML = `atOptions = ${JSON.stringify(atOptions)}`;
 
-      if (banner.current) {
-        banner.current.append(script);
-      }
+      banner.current.append(conf);
+      banner.current.append(script);
     }
-  }, []);
+  }, [banner]);
 
   return (
     <>
-      <h2>Hello</h2>
-      <div ref={banner}></div>
-      <div id={`c721be1582bda356d78a40848af09f44`}></div>
+      <h2>😠</h2>
+      <div
+        className="mx-2 my-5 border border-gray-200 justify-center items-center text-white text-center"
+        ref={banner}
+      ></div>
     </>
   );
-};
-
-export default AdsPage;
+}
